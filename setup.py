23110dbc4fd0211db04c41c6b50b2cf37be6fc0d
JorasOliveira <joras.custodio@gmail.com>
2023-08-28 09:28:44 -0300

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dev_aberto_jj',
+    version='0.1',
+    packages=['dev_aberto'],
+    author='Joras_Oliveira',               # Specify the author's name
+    python_requires='>=3.6',          # Specify the minimum Python version
+    classifiers=[
+    'Operating System :: OS Independent',  # Specify the supported operating systems
+    ]
+)
